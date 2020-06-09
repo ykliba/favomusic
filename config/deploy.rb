ce23d5415c14c21daad4fdf1cca6c73391aec83c
ykliba <mediaboxing2@gmail.com>
2020-06-09 22:10:24 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.14.0'
